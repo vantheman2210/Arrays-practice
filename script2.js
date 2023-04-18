efd54551f2fcaaf528118f5a5aa7cf62f41230ec
vantheman2210 <martinovic.ba@gmail.com>
2023-04-18 09:46:28 +0200

    Map method practice

--- script2.js
+++ script2.js
@@ -102,5 +102,12 @@ const currenciesUnique = new Set(['USD', 'GBP', 'EUR', 'USD', 'EUR']);
 console.log(currenciesUnique);
 currenciesUnique.forEach((value, _, map) => { 
   console.log(`${value}: ${value}`)
-}) 
+});
+
+const eurToUsd = 1.1;
+const movementsUsd = movements.map((mov) => { 
+	return mov * eurToUsd
+});
+console.log(movements)
+console.log(movementsUsd)
 
