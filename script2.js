7d1f668bd4cd770743c08776aa5b51396414a05f
vantheman2210 <martinovic.ba@gmail.com>
2023-04-19 12:43:42 +0200

    Sort method practice

--- script2.js
+++ script2.js
@@ -278,15 +278,17 @@ console.log(movements)
 // If we return > 0 , B , A (switch order)
 
 // Ascending
-const sortNum1 = movements.sort((a, b) => { 
-	if(a > b) return 1;
-	if (a < b) return -1;
-});
+// const sortNum1 = movements.sort((a, b) => { 
+// 	if(a > b) return 1;
+// 	if (a < b) return -1;
+// });
+const sortNum1 = movements.sort((a, b) => a - b)
 console.log(sortNum1);
 
 // Descending 
-const sortNum2 = movements.sort((a, b) => { 
-	if(a > b) return -1;
-	if (a < b) return 1;
-});
+// const sortNum2 = movements.sort((a, b) => { 
+// 	if(a > b) return -1;
+// 	if (a < b) return 1;
+// });
+const sortNum2 = movements.sort((a, b) => b - a)
 console.log(sortNum2);
